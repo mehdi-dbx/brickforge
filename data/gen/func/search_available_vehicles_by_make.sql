eb3b952f5fc779fde3ad63306f9365516d3eef4d
Mehdi Lamrani <mehdi.lamrani@databricks.com>
2026-05-12 15:33:04 +0200

    Gitignore generated data -- remove vehicle rental files from tracking
    data/gen/ output (csv, init, func, proc, manifests, wizard state)
    is user-specific and should never be shared to downstream clones.
    
    Co-authored-by: Isaac

--- data/gen/func/search_available_vehicles_by_make.sql
+++
@@ -1,9 +0,0 @@
--- Search for currently available vehicles by make, optionally filtered by city. Params: {{make}}, {{city}}
-SELECT v.vehicle_id, v.make, v.model, v.year, v.category, v.daily_rate, v.license_plate,
-       l.name AS location_name, l.city, l.state, l.airport_code
-FROM __SCHEMA_QUALIFIED__.`vehicles` v
-JOIN __SCHEMA_QUALIFIED__.`locations` l ON v.location_id = l.location_id
-WHERE LOWER(v.make) = LOWER('{{make}}')
-  AND v.is_available = TRUE
-  AND ('{{city}}' = '' OR LOWER(l.city) = LOWER('{{city}}'))
-ORDER BY v.daily_rate ASC;
--- data/gen/func/search_vehicles_by_make.sql
+++
@@ -1,6 +0,0 @@
--- Look up all vehicles matching a given make with availability status and location details. Params: {{make}}
-SELECT v.vehicle_id, v.make, v.model, v.year, v.category, v.daily_rate, v.is_available, v.license_plate, l.name AS location_name, l.city, l.state
-FROM __SCHEMA_QUALIFIED__.`vehicles` v
-LEFT JOIN __SCHEMA_QUALIFIED__.`locations` l ON v.location_id = l.location_id
-WHERE LOWER(v.make) = LOWER('{{make}}')
-ORDER BY v.year DESC, v.model ASC;
